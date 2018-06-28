ecc41cce75e3f28d7106f7c08072d8b84c755200
BetaProg <34947219+BetaProg@users.noreply.github.com>
2018-06-28 20:58:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+	<head>
+		<script type="text/javascript" src="scripts/angular.min.js"></script>
+		<link rel="stylesheet" href="styles/main_styles.css" />
+	</head>
+	<body>	
+		<div class="main_content">
+			<div class="top_content">
+				<div class="nav_bar">
+					<div class="title_and_logo">
+						<img src="images/main_logo.png" />
+						<h5>White Tiger Lubes</h5>
+					</div>
+					<div class="nav_buttons">
+						<div class="home_btn_div">
+							<button>Home</button>
+						</div>
+						<div class="products_btn_div">
+							<button>Products</button>
+						</div>
+						<div class="partners_btn_div">
+							<button>Partners</button>
+						</div>
+						<div class="about_btn_div">
+							<button>About Us</button>
+						</div>
+						<div class="contacts_btn_div">
+							<button>Contact Us</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="center_content">
+				<div class="jumbo100">
+					<div class="jumbo50 static_jumbo">
+					</div>
+					<div class="jumbo50 slider_jumbo">
+					</div>
+				</div>
+				<div class="center_bottom">
+					<div class="products_view">
+					</div>
+				</div>
+			</div>
+			<div class="footer_content">
+			</div>
+		</div>
+	</body>
+</html>
